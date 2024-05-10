ecce76f9d7824721f165e6083869aaef9e23a0cf
Ignacio Macam Jr. <ignaciomacam@gmail.com>
2024-05-10 19:36:34 +0800

    added spring security, user and role entity, and user_roles join table for database auth

---
+++ src/main/java/com/iggyzxc/todomanagement/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.iggyzxc.todomanagement.repository;
+
+import com.iggyzxc.todomanagement.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
