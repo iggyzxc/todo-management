9a6d10b5b55f03a143e1fdd2f942483b915582d9
Ignacio Macam Jr. <ignaciomacam@gmail.com>
2024-05-13 23:50:35 +0800

    added LoginDTO and login service

--- src/main/java/com/iggyzxc/todomanagement/service/AuthService.java
+++ src/main/java/com/iggyzxc/todomanagement/service/AuthService.java
@@ -1,7 +1,10 @@
 package com.iggyzxc.todomanagement.service;
 
+import com.iggyzxc.todomanagement.dto.LoginDTO;
 import com.iggyzxc.todomanagement.dto.RegisterDTO;
 
 public interface AuthService {
     String register(RegisterDTO registerDTO);
+
+    String login(LoginDTO loginDTO);
 }
--- src/main/java/com/iggyzxc/todomanagement/service/impl/AuthServiceImpl.java
+++ src/main/java/com/iggyzxc/todomanagement/service/impl/AuthServiceImpl.java
@@ -1,5 +1,6 @@
 package com.iggyzxc.todomanagement.service.impl;
 
+import com.iggyzxc.todomanagement.dto.LoginDTO;
 import com.iggyzxc.todomanagement.entity.Role;
 import com.iggyzxc.todomanagement.entity.User;
 import com.iggyzxc.todomanagement.exception.TodoAPIException;
@@ -9,6 +10,10 @@ import com.iggyzxc.todomanagement.repository.RoleRepository;
 import com.iggyzxc.todomanagement.repository.UserRepository;
 import com.iggyzxc.todomanagement.service.AuthService;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +27,7 @@ public class AuthServiceImpl implements AuthService {
     private UserRepository userRepository;
     private RoleRepository roleRepository;
     private PasswordEncoder passwordEncoder;
+    private AuthenticationManager authenticationManager;
 
     @Override
     public String register(RegisterDTO registerDTO) {
@@ -50,5 +56,16 @@ public class AuthServiceImpl implements AuthService {
         userRepository.save(user);
 
         return "User registered successfully";
+    }
+
+    @Override
+    public String login(LoginDTO loginDTO) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginDTO.getUsernameOrEmail(),
+                loginDTO.getPassword()
+        ));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return "User logged in successfully!";
     }
 }
